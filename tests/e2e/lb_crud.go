b98cadb988b47970a4b874ad3c71d4ea98432880
vincent.baer <vincent.baer@outscale.com>
2023-08-17 12:44:28 +0000

    Make createSvc and createDeployment generic and cover two example

--- tests/e2e/lb_crud.go
+++ tests/e2e/lb_crud.go
@@ -25,6 +25,7 @@ import (
 	"github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
 
+	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
@@ -34,9 +35,20 @@ import (
 
 // TestParam customize e2e tests and lb annotations
 type TestParam struct {
-	Title       string
-	Annotations map[string]string
-	Cmd         string
+	Title              string
+	Annotations        map[string]string
+	Cmd                string
+	DeploymentMetaName string
+	DeploymentName     string
+	DeploymentImage    string
+	Replicas           int32
+	Ports              []apiv1.ContainerPort
+	ServiceMetaName    string
+	ServiceName        string
+	ServicePorts       []apiv1.ServicePort
+	SourceRanges       bool
+	SourceRangesCidr   []string
+	UpdateService      bool
 }
 
 var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
@@ -55,9 +67,33 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 
 	params := []TestParam{
 		{
-			Title:       "Create LB",
-			Cmd:         "",
-			Annotations: map[string]string{},
+			Title:              "Create LB",
+			Cmd:                "",
+			Annotations:        map[string]string{},
+			DeploymentMetaName: "echoheaders",
+			DeploymentName:     "echoheaders",
+			DeploymentImage:    "gcr.io/google_containers/echoserver:1.10",
+			Replicas:           1,
+			Ports: []apiv1.ContainerPort{
+				{
+					Name:          "tcp",
+					Protocol:      apiv1.ProtocolTCP,
+					ContainerPort: 8080,
+				},
+			},
+			ServiceMetaName: "echoheaders-lb-public",
+			ServiceName:     "echoheaders",
+			ServicePorts: []v1.ServicePort{
+				{
+					Name:       "tcp",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8080),
+					Port:       80,
+				},
+			},
+			SourceRanges:     false,
+			SourceRangesCidr: []string{},
+			UpdateService:    true,
 		},
 		{
 			Title: "Create LB With proxy protocol enabled",
@@ -67,6 +103,30 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 			Annotations: map[string]string{
 				"service.beta.kubernetes.io/aws-load-balancer-proxy-protocol": "*",
 			},
+			DeploymentMetaName: "echoheaders",
+			DeploymentName:     "echoheaders",
+			DeploymentImage:    "gcr.io/google_containers/echoserver:1.10",
+			Replicas:           1,
+			Ports: []apiv1.ContainerPort{
+				{
+					Name:          "tcp",
+					Protocol:      apiv1.ProtocolTCP,
+					ContainerPort: 8080,
+				},
+			},
+			ServiceMetaName: "echoheaders-lb-public",
+			ServiceName:     "echoheaders",
+			ServicePorts: []v1.ServicePort{
+				{
+					Name:       "tcp",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8080),
+					Port:       80,
+				},
+			},
+			SourceRanges:     false,
+			SourceRangesCidr: []string{},
+			UpdateService:    true,
 		},
 		{
 			Title: "Create LB with hc customized",
@@ -77,6 +137,91 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 				"service.beta.kubernetes.io/aws-load-balancer-healthcheck-timeout":             "6",
 				"service.beta.kubernetes.io/aws-load-balancer-healthcheck-interval":            "11",
 			},
+			DeploymentMetaName: "echoheaders",
+			DeploymentName:     "echoheaders",
+			DeploymentImage:    "gcr.io/google_containers/echoserver:1.10",
+			Replicas:           1,
+			Ports: []apiv1.ContainerPort{
+				{
+					Name:          "tcp",
+					Protocol:      apiv1.ProtocolTCP,
+					ContainerPort: 8080,
+				},
+			},
+			ServiceMetaName: "echoheaders-lb-public",
+			ServiceName:     "echoheaders",
+			ServicePorts: []v1.ServicePort{
+				{
+					Name:       "tcp",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8080),
+					Port:       80,
+				},
+			},
+			SourceRanges:     false,
+			SourceRangesCidr: []string{},
+			UpdateService:    true,
+		},
+		{
+			Title:              "Create basic LB",
+			Cmd:                "",
+			Annotations:        map[string]string{},
+			DeploymentMetaName: "basic-deployment",
+			DeploymentName:     "basic",
+			DeploymentImage:    "gcr.io/google_containers/echoserver:1.10",
+			Replicas:           2,
+			Ports: []apiv1.ContainerPort{
+				{
+					Name:          "tcp",
+					Protocol:      apiv1.ProtocolTCP,
+					ContainerPort: 8080,
+				},
+			},
+			ServiceMetaName: "service-basic",
+			ServiceName:     "basic",
+			ServicePorts: []v1.ServicePort{
+				{
+					Name:       "tcp",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8080),
+					Port:       80,
+				},
+			},
+			SourceRanges:     false,
+			SourceRangesCidr: []string{},
+			UpdateService:    false,
+		},
+		{
+			Title: "Create simple lb",
+			Cmd:   "",
+			Annotations: map[string]string{
+				"service.beta.kubernetes.io/osc-load-balancer-name-length": "20",
+				"service.beta.kubernetes.io/osc-load-balancer-name":        "simple-lb-test",
+			},
+			DeploymentMetaName: "echoheaders",
+			DeploymentName:     "echoheaders",
+			DeploymentImage:    "gcr.io/google_containers/echoserver:1.10",
+			Replicas:           1,
+			Ports: []apiv1.ContainerPort{
+				{
+					Name:          "tcp",
+					Protocol:      apiv1.ProtocolTCP,
+					ContainerPort: 8080,
+				},
+			},
+			ServiceMetaName: "echoheaders-lb-advanced-public",
+			ServiceName:     "echoheaders",
+			ServicePorts: []v1.ServicePort{
+				{
+					Name:       "http",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8080),
+					Port:       80,
+				},
+			},
+			SourceRanges:     true,
+			SourceRangesCidr: []string{"0.0.0.0/0"},
+			UpdateService:    false,
 		},
 	}
 
@@ -84,13 +229,25 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 		title := param.Title
 		cmd := param.Cmd
 		annotations := param.Annotations
+		deploymentMetaName := param.DeploymentMetaName
+		deploymentName := param.DeploymentName
+		deploymentImage := param.DeploymentImage
+		replicas := param.Replicas
+		ports := param.Ports
+		serviceMetaName := param.ServiceMetaName
+		serviceName := param.ServiceName
+		servicePorts := param.ServicePorts
+		sourceRanges := param.SourceRanges
+		sourceRangesCidr := param.SourceRangesCidr
+		updateService := param.UpdateService
 		ginkgo.It(title, func() {
 			fmt.Printf("Create Simple LB :  %v\n", ns)
 			fmt.Printf("Cs :  %v\n", cs)
 			fmt.Printf("Params :  %v / %v / %v\n", title, cmd, annotations)
 
 			ginkgo.By("Create Deployment")
-			deployement := e2eutils.CreateDeployment(cs, ns, cmd)
+
+			deployement := e2eutils.CreateDeployment(cs, ns, cmd, deploymentMetaName, deploymentName, deploymentImage, replicas, ports)
 			defer e2eutils.DeleteDeployment(cs, ns, deployement)
 			defer e2eutils.ListDeployment(cs, ns)
 
@@ -101,7 +258,7 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 			e2eutils.ListDeployment(cs, ns)
 
 			ginkgo.By("Create Svc")
-			svc := e2eutils.CreateSvc(cs, ns, annotations)
+			svc := e2eutils.CreateSvc(cs, ns, annotations, serviceMetaName, serviceName, servicePorts, sourceRanges, sourceRangesCidr)
 			fmt.Printf("Created Service %q.\n", svc)
 			defer e2eutils.ListSvc(cs, ns)
 			defer e2eutils.DeleteSvc(cs, ns, svc)
@@ -129,57 +286,58 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 			fmt.Printf("address:  %v\n", address)
 
 			ginkgo.By("Test Connection (wait to have endpoint ready)")
-			e2esvc.TestReachableHTTP(address, 80, 600*time.Second)
+			e2esvc.TestReachableHTTP(address, int(servicePorts[0].Port), 600*time.Second)
+			if updateService {
+				ginkgo.By("Remove Instances from lbu")
+				elb, err := e2eutils.ElbAPI()
+				framework.ExpectNoError(err)
 
-			ginkgo.By("Remove Instances from lbu")
-			elb, err := e2eutils.ElbAPI()
-			framework.ExpectNoError(err)
+				lb, err := e2eutils.GetLb(elb, lbName)
+				framework.ExpectNoError(err)
 
-			lb, err := e2eutils.GetLb(elb, lbName)
-			framework.ExpectNoError(err)
-
-			lbInstances := []*elbApi.Instance{}
-			for _, lbInstance := range lb.Instances {
-				lbInstanceItem := &elbApi.Instance{}
-				lbInstanceItem.InstanceId = lbInstance.InstanceId
-				lbInstances = append(lbInstances, lbInstanceItem)
-			}
-			framework.ExpectNotEqual(len(lbInstances), 0)
-
-			err = e2eutils.RemoveLbInst(elb, lbName, lbInstances)
-			framework.ExpectNoError(err)
-
-			lb, err = e2eutils.GetLb(elb, lbName)
-			framework.ExpectNoError(err)
-			framework.ExpectEqual(len(lb.Instances), 0)
-
-			ginkgo.By("Add port to force update of LB")
-			port := v1.ServicePort{
-				Name:       "tcp2",
-				Protocol:   v1.ProtocolTCP,
-				TargetPort: intstr.FromInt(8443),
-				Port:       443,
-			}
-			svc = e2eutils.UpdateSvcPorts(cs, ns, updatedSvc, port)
-			fmt.Printf("svc updated:  %v\n", svc)
-
-			ginkgo.By("Test LB updated(wait to have vm registred)")
-			count = 0
-			for count < 10 {
-				lb, err = e2eutils.GetLb(elb, lbName)
-				if err == nil && len(lb.Instances) != 0 {
-					break
+				lbInstances := []*elbApi.Instance{}
+				for _, lbInstance := range lb.Instances {
+					lbInstanceItem := &elbApi.Instance{}
+					lbInstanceItem.InstanceId = lbInstance.InstanceId
+					lbInstances = append(lbInstances, lbInstanceItem)
 				}
-				count++
-				time.Sleep(30 * time.Second)
+				framework.ExpectNotEqual(len(lbInstances), 0)
+
+				err = e2eutils.RemoveLbInst(elb, lbName, lbInstances)
+				framework.ExpectNoError(err)
+
+				lb, err = e2eutils.GetLb(elb, lbName)
+				framework.ExpectNoError(err)
+				framework.ExpectEqual(len(lb.Instances), 0)
+
+				ginkgo.By("Add port to force update of LB")
+				port := v1.ServicePort{
+					Name:       "tcp2",
+					Protocol:   v1.ProtocolTCP,
+					TargetPort: intstr.FromInt(8443),
+					Port:       443,
+				}
+				svc = e2eutils.UpdateSvcPorts(cs, ns, updatedSvc, port)
+				fmt.Printf("svc updated:  %v\n", svc)
+
+				ginkgo.By("Test LB updated(wait to have vm registred)")
+				count = 0
+				for count < 10 {
+					lb, err = e2eutils.GetLb(elb, lbName)
+					if err == nil && len(lb.Instances) != 0 {
+						break
+					}
+					count++
+					time.Sleep(30 * time.Second)
+				}
+				lb, err = e2eutils.GetLb(elb, lbName)
+
+				framework.ExpectNoError(err)
+				framework.ExpectNotEqual(len(lb.Instances), 0)
+
+				ginkgo.By("TestReachableHTTP after update")
+				e2esvc.TestReachableHTTP(address, int(servicePorts[0].Port), 240*time.Second)
 			}
-			lb, err = e2eutils.GetLb(elb, lbName)
-
-			framework.ExpectNoError(err)
-			framework.ExpectNotEqual(len(lb.Instances), 0)
-
-			ginkgo.By("TestReachableHTTP after update")
-			e2esvc.TestReachableHTTP(address, 80, 240*time.Second)
 		})
 
 	}
@@ -201,6 +359,29 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 	title := "Issue #68"
 	cmd := ""
 	annotations := map[string]string{}
+	deploymentMetaName := "echoheaders"
+	deploymentImage := "gcr.io/google_containers/echoserver:1.10"
+	var replicas int32 = 1
+	deploymentName := "echoheaders"
+	serviceMetaName := "echoheaders-lb-public"
+	serviceName := "echoheaders"
+	servicePorts := []v1.ServicePort{
+		{
+			Name:       "tcp",
+			Protocol:   v1.ProtocolTCP,
+			TargetPort: intstr.FromInt(8080),
+			Port:       80,
+		},
+	}
+	sourceRanges := false
+	sourceRangesCidr := []string{}
+	ports := []apiv1.ContainerPort{
+		{
+			Name:          "tcp",
+			Protocol:      apiv1.ProtocolTCP,
+			ContainerPort: 8080,
+		},
+	}
 	ginkgo.It(title, func() {
 		nsSvc1, err := f.CreateNamespace("svc1", map[string]string{})
 		framework.ExpectNoError(err)
@@ -210,12 +391,14 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 		fmt.Printf("Params :  %v / %v / %v\n", title, cmd, annotations)
 
 		ginkgo.By("Create Deployment 1")
-		deployementSvc1 := e2eutils.CreateDeployment(cs, nsSvc1, cmd)
+
+		deployementSvc1 := e2eutils.CreateDeployment(cs, nsSvc1, cmd, deploymentMetaName, deploymentName, deploymentImage, replicas, ports)
 		defer e2eutils.DeleteDeployment(cs, nsSvc1, deployementSvc1)
 		defer e2eutils.ListDeployment(cs, nsSvc1)
 
 		ginkgo.By("Create Deployment 2")
-		deployementSvc2 := e2eutils.CreateDeployment(cs, nsSvc2, cmd)
+
+		deployementSvc2 := e2eutils.CreateDeployment(cs, nsSvc2, cmd, deploymentMetaName, deploymentName, deploymentImage, replicas, ports)
 		defer e2eutils.DeleteDeployment(cs, nsSvc1, deployementSvc2)
 		defer e2eutils.ListDeployment(cs, nsSvc1)
 
@@ -228,12 +411,12 @@ var _ = ginkgo.Describe("[ccm-e2e] SVC-LB", func() {
 		e2eutils.ListDeployment(cs, nsSvc2)
 
 		ginkgo.By("Create Svc 1")
-		svc1 := e2eutils.CreateSvc(cs, nsSvc1, annotations)
+		svc1 := e2eutils.CreateSvc(cs, nsSvc1, annotations, serviceMetaName, serviceName, servicePorts, sourceRanges, sourceRangesCidr)
 		fmt.Printf("Created Service %q.\n", svc1)
 		defer e2eutils.ListSvc(cs, nsSvc1)
 
 		ginkgo.By("Create Svc 2")
-		svc2 := e2eutils.CreateSvc(cs, nsSvc2, annotations)
+		svc2 := e2eutils.CreateSvc(cs, nsSvc2, annotations, serviceMetaName, serviceName, servicePorts, sourceRanges, sourceRangesCidr)
 		fmt.Printf("Created Service %q.\n", svc2)
 		defer e2eutils.ListSvc(cs, nsSvc2)
 		defer e2eutils.DeleteSvc(cs, nsSvc2, svc2)
